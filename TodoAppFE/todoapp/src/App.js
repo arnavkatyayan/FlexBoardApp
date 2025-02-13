74b37b22d3f02004968b1190e64a37fe6b389620
Arnav Katyayan <ishumisra99@gmail.com>
2025-02-13 15:25:52 +0530

    Light-mode dark-mode feature.

--- TodoAppFE/todoapp/src/App.js
+++ TodoAppFE/todoapp/src/App.js
@@ -4,32 +4,44 @@ import Logo from './MainIcon.png';
 import { Button } from 'react-bootstrap';
 import SignupPage from './SignupPage';
 import { useState } from 'react';
+import { Switch, FormControlLabel } from "@mui/material";
+import LightMode from './light-mode.png';
+import DarkMode from './dark-mode.png';
+
 function App() {
-    const [isSignupClicked, setIsSignUpClicked]= useState(false);
+  const [isSignupClicked, setIsSignUpClicked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
-    const handleSignup = ()=> {
-      setIsSignUpClicked(!isSignupClicked);
-    }
+  const handleSignup = () => {
+    setIsSignUpClicked(!isSignupClicked);
+  }
 
-   
+  const handleSwitch = (event) => {
+    setChecked(event.target.checked);
+  }
 
   return (
-    <div className='bg-color'>
-      <div className="logo-container bg-color">
+    <div className={checked? 'bg-color':null}>
+      <div className={`logo-container ${checked ? 'bg-color':null}`}>
         <div className="logo">
           <img src={Logo} className="icon" alt="App Logo" />
-          <span className="app-name">FlexBoard</span>
+          <span className={`app-name ${checked ? 'bg-color-labels' : ''}`}>FlexBoard</span>
         </div>
         <div className="other-features">
-          <h4>Pricing</h4>
-          <h4 onClick={handleSignup}>Signup</h4>
+          <FormControlLabel className='switch'
+            control={<Switch checked={checked} onChange={handleSwitch} />}
+            label={checked ? <img src={LightMode} className="switch-icons" /> : <img src={DarkMode} className="switch-icons" />}
+          />
+          <h4 className={`pricing ${checked ? 'bg-color-labels' : ''}`}>Pricing</h4>
+          <h4 className={`signup ${checked ? 'bg-color-labels' : ''}`} onClick={handleSignup}>Signup</h4>
+
         </div>
       </div>
       {isSignupClicked && <SignupPage />}
-      {!isSignupClicked ? 
-      <div className="App">
-        <LoginPage />
-      </div> :null }
+      {!isSignupClicked ?
+        <div className="App">
+          <LoginPage />
+        </div> : null}
     </div>
   );
 }
