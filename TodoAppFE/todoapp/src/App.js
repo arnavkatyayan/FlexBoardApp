948c04dc6c56a9c18ce57dd2ae2da09a2fd3e3e0
Arnav Katyayan <ishumisra99@gmail.com>
2025-04-06 20:39:38 +0530

    Adding Logout and change password functionality

--- TodoAppFE/todoapp/src/App.js
+++ TodoAppFE/todoapp/src/App.js
@@ -12,52 +12,63 @@ import About from './About';
 import ForgetPassword from './ForgetPassword';
 import axios from 'axios';
 import TodoList from './TodoList';
-import Coins from './coin.png'
+import Coins from './coin.png';
+
 function App() {
- 
   const [checked, setChecked] = useState(false);
   const [activePage, setActivePage] = useState("login");
   const [coins, setCoins] = useState(0);
   const [isCoinDisplayed, setIsCoinDisplayed] = useState(false);
-  const handleSignup = () => {
-    setActivePage("signup")
-  }
 
+  const handleSignup = () => {
+    setActivePage("signup");
+  };
+
+  const handleLogout = () => {
+    setActivePage("login");
+  }
+    
   const handleSwitch = (event) => {
     setChecked(event.target.checked);
-  }
+  };
 
   const handlePricing = () => {
-    setActivePage("pricing")
-  }
+    setActivePage("pricing");
+  };
 
   const handleAbout = () => {
     setActivePage("about");
-  }
+  };
+
   const handleForgetPassword = () => {
     setActivePage("forget-password");
+  };
+
+  const handleChangePassword = () => {
+    setActivePage("change-password");
   }
 
   const checkEmailAvailable = async (email) => {
     const emailData = {
-        email: email.trim()
-    }
+      email: email.trim()
+    };
     const response = await axios.post('http://127.0.0.1:5000/check_email_exists', emailData);
     return response.data;
-}
+  };
 
   const handleLoginOnLogoClick = () => {
     setActivePage("login");
-  }
+  };
 
   return (
-    <div className={checked? 'bg-color':null}>
-      <div className={`logo-container ${checked ? 'bg-color':null}`}>
+    <div className={checked ? 'bg-color' : null}>
+      <div className={`logo-container ${checked ? 'bg-color' : null}`}>
         <div className="logo">
           <img src={Logo} className="icon" alt="App Logo" />
           <span className={`app-name cursor-pointer ${checked ? 'bg-color-labels' : ''}`} onClick={handleLoginOnLogoClick}>FlexBoard</span>
           <div className='coins-flex'>
-          {isCoinDisplayed ? <img src={Coins} className='coins-css' /> : null} {isCoinDisplayed ? <span className='coin-number'>{coins}</span> : null}
+            {isCoinDisplayed ? <img src={Coins} className='coins-css' /> : null}
+            {isCoinDisplayed ? <span className='coin-number'>{coins}</span> : null}
           </div>
         </div>
         <div className="other-features">
@@ -66,13 +77,12 @@ function App() {
             label={checked ? <img src={LightMode} className="switch-icons" /> : <img src={DarkMode} className="switch-icons" />}
           /> */}
           <h4 className={`pricing ${checked ? 'bg-color-labels' : ''}`} onClick={handlePricing}>Prices</h4>
-          <h4 className={`signup ${checked ? 'bg-color-labels' : ''}`} onClick={handleForgetPassword}>Forget Password</h4>
-          <h4 className={`signup ${checked ? 'bg-color-labels' : ''}`} onClick={handleSignup}>Signup</h4>
+          {!isCoinDisplayed ? <h4 className={`signup ${checked ? 'bg-color-labels' : ''}`} onClick={handleForgetPassword}>Forget Password</h4> : <h4 className={`signup ${checked ? 'bg-color-labels' : ''}`} onClick={handleChangePassword}>Change Password</h4>}
+          {!isCoinDisplayed ? <h4 className={`signup ${checked ? 'bg-color-labels' : ''}`} onClick={handleSignup}>Signup</h4> : <h4 className={`signup ${checked ? 'bg-color-labels' : ''}`} onClick={handleLogout}>Logout</h4>}
           <h4 className={`pricing ${checked ? 'bg-color-labels' : ''}`} onClick={handleAbout}>About</h4>
-
         </div>
       </div>
-      {activePage === 'login' && <LoginPage coins={coins} setCoins={setCoins} setIsCoinDisplayed={setIsCoinDisplayed}/>}
+      {activePage === 'login' && <LoginPage coins={coins} setCoins={setCoins} setIsCoinDisplayed={setIsCoinDisplayed} setUser={setUser} />}
       {activePage === 'signup' && <SignupPage checkEmailAvailable={checkEmailAvailable} />}
       {activePage === 'pricing' && <Pricing />}
       {activePage === 'about' && <About />}
