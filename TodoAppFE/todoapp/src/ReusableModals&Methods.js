7c72b0890532104d8bcea2e1299d65b337e70baf
Arnav Katyayan <ishumisra99@gmail.com>
2025-05-04 22:13:56 +0530

    Further changes on view project part.

--- TodoAppFE/todoapp/src/ReusableModals&Methods.js
+++ TodoAppFE/todoapp/src/ReusableModals&Methods.js
@@ -2,15 +2,29 @@ import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
 
-export const reusableModalViewProject = (showModal, handleClose, title) => {
+export const ReusableModalViewProject = ({ showModal, handleClose, project }) => {
+
     return (
-      <Modal show={showModal} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          
-        </Modal.Body>
-      </Modal>
+        <Modal show={showModal} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{project.project_name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p><b>Description:</b><span className="project-description "> {project.description} </span></p>
+                <p><b>Start Date:</b> {project.start_date}</p>
+                <p><b>Deadline:</b> {project.end_date}</p>
+                <p ><b>Priority:</b> <span
+                    className={
+                        project.priority === "low"
+                            ? "project-low"
+                            : project.priority === "medium"
+                                ? "project-medium"
+                                : "project-high"
+                    }
+                >
+                    {project.priority}
+                </span></p>
+            </Modal.Body>
+        </Modal>
     );
-  };
+};
--- TodoAppFE/todoapp/src/ViewProject.js
+++ TodoAppFE/todoapp/src/ViewProject.js
@@ -2,46 +2,58 @@ import React from "react";
 import {useState,useEffect} from "react";
 import axios from "axios";
 import swal from "sweetalert";
-import { reusableModalViewProject } from "./ReusableModals&Methods";
+import { ReusableModalViewProject } from "./ReusableModals&Methods";
 import { Button } from "react-bootstrap";
 
 function ViewProject(props) {
     const [projects, setProjects] = useState([]);
     const [showModal, setShowModal] = useState(false);
+    const [selectedProject, setSelectedProject] = useState(null); // NEW
 
-    const getProjects = async ()=> {
-        try {
-        const response = await axios.get("http://127.0.0.1:5000/getProjectDetails",{
-            params: { userName:props.userName }, 
-        });
-       setProjects(response.data.projects);
-        }
-        catch(error) {
-            console.log("error fetching projects");
-        }
-    }
-
-    useEffect(()=> {
-        const checkProjectAvailability = async () => {
-             await getProjects();
+    useEffect(() => {
+        const getProjects = async () => {
+            try {
+                const response = await axios.get("http://127.0.0.1:5000/getProjectDetails", {
+                    params: { userName: props.userName },
+                });
+                setProjects(response.data.projects);
+            } catch (error) {
+                console.log("error fetching projects");
+            }
         };
-        checkProjectAvailability();
-    },[]);
+        getProjects();
+    }, []);
+
+    const handleClose = () => {
+        setShowModal(false);
+        setSelectedProject(null);
+    };
+
+    const handleShowProject = (project) => {
+        setSelectedProject(project);
+        setShowModal(true);
+    };
 
     return (
-        
         <div className="project-view">
             {projects.map((project, index) => (
-                <div className="project-individual-css">
+                <div className="project-individual-css" key={index}>
                     <div className="project-title">{project.project_name}</div>
                     <div className="btn-grps btn-grps-view-project">
                         <Button>Open Project</Button>
-                        <Button>Show Project</Button>
+                        <Button onClick={() => handleShowProject(project)}>Project Details</Button>
                         <Button>Delete Project</Button>
-                        </div> 
+                    </div>
                 </div>
             ))}
 
+            {selectedProject && (
+                <ReusableModalViewProject
+                    showModal={showModal}
+                    handleClose={handleClose}
+                    project={selectedProject} 
+                />
+            )}
         </div>
- )   
+    );
 }export default ViewProject;
